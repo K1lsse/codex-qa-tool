c578dfe8b3c1f81f760096f02a3ae6706f61f106
Jason <wjxzxc11233@outlook.com>
2026-06-29 20:49:31 +0800

    push QA_Codex full project v0.2

--- Codex_advice/analyzer.py
+++
@@ -1,7 +0,0 @@
-from client import call_codex
-from prompts import build_log_prompt
-
-def analyze_log(log_text: str):
-    prompt = build_log_prompt(log_text)
-    result = call_codex(prompt)
-    return result
